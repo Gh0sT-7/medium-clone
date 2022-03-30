a95ef492a61bf04feb48d2129c1d7600a1740059
James <jamie_c12@hotmail.com>
2022-03-30 08:13:16 +0100

    Added post to the homepage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,44 +5,72 @@ import Header from '../components/Header';
 import Banner from '../components/Banner'
 import { sanityClient, urlFor } from "../sanity";
 import { Post } from '../typings';
+import Link from "next/link";
 
 interface Props {
-	post: [Post];
+	posts: [Post];
 }
 
 export default function Home({ posts }: Props) {
-	console.log({posts})
 	return (
-		<div className="max-w-7xl mx-auto">
-			<Head>
-				<title>Medium Blog</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+    <div className="mx-auto max-w-7xl">
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<Header />
-			<Banner />
-		</div>
-	)
+      <Header />
+      <Banner />
+
+      <div>
+        {posts.map((post) => (
+          <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+              <div className="border rounded-lg group cursor-pointer overflow-hidden">
+                <img
+                  className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                  src={urlFor(post.mainImage).url()!}
+                />
+                <div className="flex justify-between bg-white p-5">
+                  <div>
+                    <p className="text-lg font-bold">{post.title}</p>
+                    <p className="text-xs">
+                      {post.description} by {post.author.name}
+                    </p>
+                  </div>
+
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src={urlFor(post.author.image).url()!}
+                  />
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export const getServerSideProps = async () => {
-	const query = `*[_type == "post"] {
-		_id,
-		title,
-		author -> {
-			name,
-			image
-		},
-		description,
-		mainImage,
-		slug
-	}`;
+const query = `*[_type == "post"] {
+_id,
+title,
+author -> {
+name,
+image
+},
+description,
+mainImage,
+slug
+}`;
 
-	const posts = await sanityClient.fetch(query);
+const posts = await sanityClient.fetch(query);
 
-	return {
-		props: {
-			posts,
-		}
-	}
+return {
+props: {
+posts,
+}
+}
 }
--- sanity.js
+++ sanity.js
@@ -1,18 +1,15 @@
-import {
-	createImageUrlBuilder,
-	createCurrentUserHook,
-	createClient,
-} from "next-sanity";
+import { createCurrentUserHook, createClient, } from "next-sanity";
+import createImageUrlBuilder from "@sanity/image-url"
 
 export const config = {
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 	apiVersion: "2021-10-21",
-	useCDN: process.env.NODE_ENV === "production",
+	useCdn: process.env.NODE_ENV === "production",
 }
 
 export const sanityClient = createClient(config);
 
 export const urlFor = (source) => createImageUrlBuilder(config).image(source);
 
-// export const useCurrentUser = creatCurrentUserHook(config);
+export const useCurrentUser = createCurrentUserHook(config);
